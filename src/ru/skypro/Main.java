efaac00b1ff40009c0fe0672e101a02938e06693
Micelovecat <flora_bux@mail.ru>
2021-11-20 04:51:38 +0300

    Second commit

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -4,5 +4,58 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int a = 3;
+        float b = 5.1f;
+        long c = 4L;
+        byte d = 1;
+        short e = 2;
+        double f = 6.1;
+        char g = 0;
+        boolean h = d > e;
+        System.out.println("HomeWork2. Exercise 1.");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+        System.out.println("e = " + e);
+        System.out.println("f = " + f);
+        System.out.println("g = " + g);
+        System.out.println("h = " + h);
+
+        float firstBoxer = 78.2f;
+        float secondBoxer = 82.7f;
+        System.out.println("HomeWork2. Exercise 2.");
+        System.out.println("Общий вес бойцов = " + (firstBoxer + secondBoxer) + " кг");
+        System.out.println("Разница в весе = " + (secondBoxer - firstBoxer) + " кг");
+
+        int bananas = 80;
+        float milk = 105 / 100f;
+        int icecream = 100;
+        int eggs = 70;
+        float mixed = (bananas * 5f) + (milk * 200f) + (icecream * 2f) + (eggs * 4f);
+        float mixedKg = mixed / 1000f;
+        System.out.println("HomeWork2. Exercise 3.");
+        System.out.println("Общий вес завтрака = " + mixed + " грамм");
+        System.out.println("Общий вес завтрака " + mixedKg + " кг");
+
+        int weightOff = 7 * 1000;
+        int value250InDay = weightOff / 250;
+        int value500InDay = weightOff / 500;
+        int middleSuccessfulDay = (value500InDay + value250InDay) / 2 % weightOff;
+        System.out.println("HomeWork2. Exercise 4.");
+        System.out.println("Сброс веса в день по  250 гр = " + value250InDay + " дней");
+        System.out.println("Сброс веса в день по  500 гр = " + value500InDay + " дней");
+        System.out.println("Среднее количество дней = " + middleSuccessfulDay);
+
+
+        int sotrudnik =
+        float masha = 67_760;
+        float denis = 83_690;
+        float christina = 76_230;
+        float
+        System.out.println("HomeWork2. Exercise 5.");
+
+
+
     }
 }
